9d825c09adb8b1cec28d4e975d2a7f45b119890d
P3ridot <blackxxoj@gmail.com>
2020-08-10 19:15:46 +0200

    Remove center fun from Location.kt

--- api/src/main/kotlin/eu/feather/featherengine/Location.kt
+++ api/src/main/kotlin/eu/feather/featherengine/Location.kt
@@ -1,7 +1,5 @@
 package eu.feather.featherengine
 
-import kotlin.math.floor
-
 data class Location(
     var world: World,
     var x: Double,
@@ -10,11 +8,5 @@ data class Location(
     var pitch: Float = 0F,
     var yaw: Float = 0F
 ) {
-
-    fun center() {
-        x = floor(x) + 0.5
-        y = floor(y) + 0.5
-        z = floor(z) + 0.5
-    }
 
 }
