3c49103c50e284245a901877a240dae5a991fcea
P3ridot <blackxxoj@gmail.com>
2020-08-10 19:34:45 +0200

    Fix Array in CommandSender.kt

--- api/src/main/kotlin/eu/feather/featherengine/command/CommandSender.kt
+++ api/src/main/kotlin/eu/feather/featherengine/command/CommandSender.kt
@@ -1,13 +1,10 @@
 package eu.feather.featherengine.command
-
-import com.sun.xml.internal.fastinfoset.util.StringArray
-
 interface CommandSender {
 
     val name: String
 
     fun sendMessage(message: String)
 
-    fun sendMessage(message: StringArray)
+    fun sendMessage(message: Array<String>)
 
 }
