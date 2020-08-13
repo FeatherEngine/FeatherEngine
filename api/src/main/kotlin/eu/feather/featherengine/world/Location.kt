09e45165a7d78a5d62ab536146d376bb1d6f6dd5
P3ridot <blackxxoj@gmail.com>
2020-08-13 15:56:44 +0200

    Functions to compare distance between two locations

--- api/src/main/kotlin/eu/feather/featherengine/world/Location.kt
+++ api/src/main/kotlin/eu/feather/featherengine/world/Location.kt
@@ -1,5 +1,9 @@
 package eu.feather.featherengine.world
 
+import eu.feather.featherengine.exceptions.DifferentWorldsException
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 data class Location(
     var world: World,
     var x: Double,
@@ -8,5 +12,21 @@ data class Location(
     var pitch: Float = 0F,
     var yaw: Float = 0F
 ) {
+
+    fun distanceFlat(location: Location): Double {
+        if (world != location.world) {
+            throw DifferentWorldsException("Locations are in two different worlds")
+        }
+
+        return sqrt((x - location.x).pow(2) + (z - location.z).pow(2))
+    }
+
+    fun distance(location: Location): Double {
+        if (world != location.world) {
+            throw DifferentWorldsException("Locations are in two different worlds")
+        }
+
+        return sqrt(distanceFlat(location).pow(2) + (y - location.y).pow(2))
+    }
 
 }
--- engine/src/main/kotlin/eu/feather/featherengine/network/util/VarInt.kt
+++ engine/src/main/kotlin/eu/feather/featherengine/network/util/VarInt.kt
@@ -4,10 +4,12 @@ import eu.feather.featherengine.network.readVarInt
 import io.ktor.utils.io.ByteReadChannel
 
 class VarInt {
+
     var value = 0
         private set
 
     suspend fun parse(channel: ByteReadChannel) {
         value = channel.readVarInt()
     }
+
 }
