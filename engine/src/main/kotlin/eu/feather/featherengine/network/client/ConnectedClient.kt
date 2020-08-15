9cb36db3db5594a3d1909025487579c3e52fe194
kittenfromvoid <etooilikespizza@gmail.com>
2020-08-15 19:37:12 +0200

    basic client stuff

---
+++ engine/src/main/kotlin/eu/feather/featherengine/network/client/ConnectedClient.kt
@@ -0,0 +1,22 @@
+package eu.feather.featherengine.network.client
+
+import eu.feather.featherengine.architecture.parse
+import eu.feather.featherengine.network.packets.HandShakePacket
+import io.ktor.network.sockets.Socket
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
+
+class ConnectedClient(
+    socket: Socket
+) {
+
+    val input = socket.openReadChannel()
+    val output = socket.openWriteChannel()
+
+    var state = ConnectionState.HANDSHAKING
+
+    suspend fun handle() {
+        input.parse(HandShakePacket)
+    }
+
+}
